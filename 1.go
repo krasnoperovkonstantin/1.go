aadf2f8c6a655cc7153009b7d843454e877c6514
krasnoperovkonstantin <58658678+krasnoperovkonstantin@users.noreply.github.com>
2020-03-02 21:55:35 +0400

    Выданные замечания устранены
    1. Вывод ошибок сделан в StdErr;
    2. Исключен конкурентный доступ на обновление переменной total.

--- 1.go
+++ 1.go
@@ -13,38 +13,53 @@ import (
 func getcontent(url string) string {
    resp, err := http.Get(url)
    if err != nil {
-      fmt.Println(err)
+      fmt.Fprintln(os.Stderr, err)
       return ""
    }
    defer resp.Body.Close()
    body, err := ioutil.ReadAll(resp.Body)
    if err != nil {
-      fmt.Println(err)
+      fmt.Fprintln(os.Stderr, err)
       return ""
    }
    return string(body)
 }
 
-func process(url string) int {
+func process(url string, ch_total chan int) {
    count := strings.Count(getcontent(url), "Go")
    fmt.Printf("Count for %s: %d\n", url, count)
-   return count
+   ch_total <- count
 }
 
-func main() { 
+func main() {
    var wg sync.WaitGroup
    ch := make(chan bool, 5)
+   ch_total := make (chan int)
+   ch_done := make (chan bool)
    var total int
+   go func () {
+      for true {
+         count, flag := <- ch_total
+         if flag {
+            total += count
+         } else {
+            fmt.Printf("Total: %d", total)
+            ch_done <- true
+            return
+         }
+      }
+   }()
    scanner := bufio.NewScanner(os.Stdin)
    for scanner.Scan() {
       ch <- true
       wg.Add(1)
       go func(url string) {
          defer wg.Done()
-         total += process (url)
+         process (url, ch_total)
          <- ch
       }(scanner.Text())
    }
    wg.Wait()
-   fmt.Printf("Total: %d",total)
+   close(ch_total)
+   <- ch_done
 }
